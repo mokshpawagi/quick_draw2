38bd9a5a2cfa0cf79eca73dffeece4bad12959ef
mokshpawagi <88577365+mokshpawagi@users.noreply.github.com>
2022-01-02 11:04:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+quick_draw_data_set=[sketch_name];
+
+random_no = Math.floor((Math.random()*array_1.length)+1);
+sketch_name = Math.floor((Math.random()*array_1.length)+1);
+console.log(random_no);
+Element_of_array = array_1[random_no];
+
+var timer_counter = 0;
+var timer_check = "";
+var drawn_sketch = "";
+var awnser_holder = "";
+var score = 0;
